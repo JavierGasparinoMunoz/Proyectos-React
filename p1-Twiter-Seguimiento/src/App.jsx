55be9d60d32097d5b8a31ef4beca8871dc506d8d
Javier <47026676+JavierGasparinoMunoz@users.noreply.github.com>
2023-10-08 20:47:51 +0200

    Commit Inicial Seguimiento en Twitter

---
+++ p1-Twiter-Seguimiento/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'SocialDrive_es',
+    name: 'SocialDrive',
+    isFollowing: false
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
